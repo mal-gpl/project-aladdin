72375d8e05c8ce49daffb5146ac799c21c5f72e9
Mario <mario.grimaldi89@gmail.com>
2012-03-17 10:29:57 +0100

    First commit

---
+++ docroot/WEB-INF/src/it/mal/portlet/controller/CoreController.java
@@ -0,0 +1,11 @@
+package it.mal.portlet.controller;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class CoreController
+ */
+public class CoreController extends MVCPortlet {
+ 
+
+}
